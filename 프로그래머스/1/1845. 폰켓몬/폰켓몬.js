1fcddef8b1cd9465b546faa5993f6999d1f917f7
Hyunw00 <96560273+kodingzz@users.noreply.github.com>
2025-02-10 22:21:20 +0900

    [level 1] Title: 폰켓몬, Time: 0.81 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,10 +1,13 @@
-
- function solution(nums) {
+// n마리중 2/n번 뽑아야함
+// 문제는 중복되지 않는 포켓몬들을 최대한 많이 뽑아야된다.
+//  중복을 제거한 후의 포켓몬과 n/2을 비교
+// 만약 서로다른 포켓몬의 갯수 >= n/2  ->n/2개
+// 서다른 포켓몬의 갯수 < n/2 -> 서로다른 포켓몬의 갯수
+function solution(nums) {
+  const poketmon = new Set(nums);
   const pickCount = Math.floor(nums.length / 2);
 
-  return new Set(nums).size < pickCount ? new Set(nums).size : pickCount;
+  return poketmon.size >= pickCount ? pickCount : poketmon.size;
 }
 
 console.log(solution([3, 1, 2, 3]));
-console.log(solution([3, 3, 3, 2, 2, 4]));
-console.log(solution([3, 3, 3, 2, 2, 2]));
