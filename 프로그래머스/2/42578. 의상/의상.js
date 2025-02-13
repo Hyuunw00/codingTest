1e2adb7c96f8df827a4e10b016b2fb80b7dff05e
현우 <96560273+Hyuunw00@users.noreply.github.com>
2025-02-13 23:23:01 +0900

    [level 2] Title: 의상, Time: 0.16 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -7,13 +7,18 @@
 
 */
 function solution(clothes) {
-  const map = new Map();
-  for (i = 0; i < clothes.length; i++) {
-    if (!map.get(clothes[i][1])) map.set(clothes[i][1], []);
-    map.get(clothes[i][1]).push(clothes[i][0]);
-  }
-  const totalClothes = [...map.values()];
-  return totalClothes.reduce((prev, cur) => prev * (cur.length + 1), 1) - 1;
+  //   const map = new Map();
+  //   for (i = 0; i < clothes.length; i++) {
+  //     if (!map.get(clothes[i][1])) map.set(clothes[i][1], []);
+  //     map.get(clothes[i][1]).push(clothes[i][0]);
+  //   }
+  const totalClothes = Object.values(
+    clothes.reduce((prev, cur) => {
+      prev[cur[1]] = prev[cur[1]] ? prev[cur[1]] + 1 : 1;
+      return prev;
+    }, {})
+  );
+  return totalClothes.reduce((prev, cur) => prev * (cur + 1), 1) - 1;
 }
 
 console.log(
@@ -30,3 +35,4 @@ console.log(
     ["smoky_makeup", "face"],
   ])
 );
+
