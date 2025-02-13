89549143f072508992d886238ac92cc7a58e6995
현우 <96560273+Hyuunw00@users.noreply.github.com>
2025-02-13 23:11:01 +0900

    [level 2] Title: 의상, Time: 0.17 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -0,0 +1,32 @@
+/* 
+서로다른 조합의 의상을 입을 경우의수를 구하는 문제
+전체 경우의수에서 조합으로 치지 않는 경우의수를 제외하면 된다.
+조합이 안되는 경우는 아무것도 입지 않는 경우이다.
+
+전체 경우의수를 구해서 -1을 한다.
+
+*/
+function solution(clothes) {
+  const map = new Map();
+  for (i = 0; i < clothes.length; i++) {
+    if (!map.get(clothes[i][1])) map.set(clothes[i][1], []);
+    map.get(clothes[i][1]).push(clothes[i][0]);
+  }
+  const totalClothes = [...map.values()];
+  return totalClothes.reduce((prev, cur) => prev * (cur.length + 1), 1) - 1;
+}
+
+console.log(
+  solution([
+    ["yellow_hat", "headgear"],
+    ["blue_sunglasses", "eyewear"],
+    ["green_turban", "headgear"],
+  ])
+);
+console.log(
+  solution([
+    ["crow_mask", "face"],
+    ["blue_sunglasses", "face"],
+    ["smoky_makeup", "face"],
+  ])
+);
