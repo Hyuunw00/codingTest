87e6d74bfc293bf20bf4b6a123c5438780304e7d
현우 <96560273+Hyuunw00@users.noreply.github.com>
2025-03-19 00:17:25 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 79.66 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,23 +1,33 @@
-import java.util.Arrays;
+import java.util.HashMap;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-         Arrays.sort(participant);
-         Arrays.sort(completion);
+        String result ="";
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for (String player : participant) {
+            map.put(player, map.getOrDefault(player, 0) + 1);
+        }
 
-         for(int i=0;i<completion.length;i++){
-            if(!participant[i].equals(completion[i])) return participant[i];
-         }
-         return participant[participant.length-1];
+       for (String player : completion) {
+            map.put(player, map.get(player)-1);
+        }
+
+        for(String key : map.keySet()){
+            if(map.get(key)!=0) {
+                result= key;
+                break;
+            }
+        }
+        
+        return result; 
     }
 
-    public static void main(String[] args){
-        String[] part= {"leo", "kiki", "eden"};
-        String[] com = {"eden", "kiki"};
+    public static void main(String[] args) {
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"eden", "kiki"};
 
-        Solution sol=  new Solution();
-        System.out.println(sol.solution(part,com));
+        Solution sol = new Solution();
+        System.out.println(sol.solution(participant, completion));  // 메서드 호출
     }
 }
-
-
