a0419d82b3d05a6b272fab90485dcfe6a6d845e6
현우 <96560273+Hyuunw00@users.noreply.github.com>
2025-03-23 17:55:48 +0900

    [level 2] Title: 기능개발, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,6 +1,6 @@
 function solution(progresses, speeds) {
     const deployResult= [];
-    // [7,3,9];
+    // [5, 10, 1, 1, 20, 1]
     
     // 1.각 기능마다 몇일만에 기능이 완성되는지 파악한다.
      const restDay= progresses.map((item,index)=>{
@@ -8,20 +8,19 @@ function solution(progresses, speeds) {
      })
     
     let count=1;
-    let maxDay=restDay[0];
+    let maxDay=0;
     // 2. 현재 기능 요일과 다음 기능의 요일 크기를 비교한다.
-    for(let i=1;i<restDay.length;i++){
+    for(let i=0;i<restDay.length;i++){
         
-        if(maxDay>=restDay[i]){
+        if(Math.max(restDay[i],maxDay)>=restDay[i+1]){
             count++;
+            maxDay= Math.max(restDay[i],maxDay);
         }
         else{
             deployResult.push(count);
             count=1;
-            maxDay= restDay[i];
         }
     }
-    deployResult.push(count);
-
+    
     return deployResult;
 }
