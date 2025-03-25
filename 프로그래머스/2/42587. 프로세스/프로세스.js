6150585422bafcf4c024ca6fc510a17dec11e26e
현우 <96560273+Hyuunw00@users.noreply.github.com>
2025-03-26 00:30:10 +0900

    [level 2] Title: 프로세스, Time: 0.40 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,26 +1,25 @@
 function solution(priorities, location) {
     
-    
-    
     // 1. priorities에 따른 인덱스를 저장할 배열을 만든다.
     const indexes= Array.from({length:priorities.length},(_,index)=>index);
     let count =1;
     
     // 2.  location과 index가 같을때까지 반복하면서 count값을 증가시킨다.
     while(true){
+         const shiftPri= priorities.shift();
+         const shiftIndex= indexes.shift();
         // priorities[0]가 우선순위가 젤 높다면
-        if(priorities[0] >= Math.max(...priorities.slice(1))){
+        const maxPri = Math.max(...priorities);
+        if(shiftPri >= maxPri){
         // 만약 index값이 location이랑 같다면 return count
-            if(indexes[0]===location) return count;
-        // 같지 않으면 그냥 shift
-         priorities.shift();
-         indexes.shift();
+            if(shiftIndex===location) return count;
+        // 같지 않으면 count++
          count++;
         }
         // 우선순위가 젤 높지 않다면
         else{
-        priorities.push(priorities.shift());
-        indexes.push(indexes.shift());
+        priorities.push(shiftPri);
+        indexes.push(shiftIndex);
         }
     }
     // 3. count값을 반환한다.
