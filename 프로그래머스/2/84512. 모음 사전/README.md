2f34e4ad352c66e8d65395dabf7fd47733814bdd
현우 <96560273+Hyuunw00@users.noreply.github.com>
2025-04-05 19:57:30 +0900

    [level 2] Title: 모음 사전, Time: 8.34 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
 
 ### 성능 요약
 
-메모리: 36.6 MB, 시간: 9.15 ms
+메모리: 36.1 MB, 시간: 8.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 05일 19:42:41
+2025년 04월 05일 19:57:24
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,40 +1,24 @@
 function solution(targetWord) {
- /*
- 
- 1. 아이디어
- A~ UUUUU 까지 모든 경우의 수를 배열에 저장해놓고
- 주어진 word의 index를 파악한다. 
- 순서를 구해야하기 때문에 +1해준다.
- 
- 2. 시간복잡도
- 각자릿수마다 올 수 있는 경우의수는 6가지(아무 단어도 안쓸때 포함)이므로 단어를 만들 수 있는 전체 경우의 수는 6^5 
- 3. 자료구조
- 전체 단어 조합 배열
-*/
-    const totalWord= [];
-    const alphabets= ['A','E','I','O','U','X'];
+    const alphabet= ['A','E','I','O','U'];
+    let flag=false;
+    let count=0;
+    dfs('');
     
-    let word='';
-    for(let i=0;i<alphabets.length;i++){
-        word='';
-        makeWord(word,alphabets[i]);
-    }
-    
-    function makeWord(beforeWord, alphabet){
-        beforeWord+=alphabet;
-        
-        if(beforeWord.length===5) {
-            totalWord.push(beforeWord);
+  
+    function dfs(cur){
+        if(cur.length>5 || flag) return;
+        if(cur===targetWord){
+            flag=true;
             return;
         }
-        for(let i=0;i<alphabets.length;i++){
-            makeWord(beforeWord,alphabets[i]);
+        count++;
+        for(let i=0;i<alphabet.length;i++){
+            dfs(cur+alphabet[i]);
         }
+        
     }
-    const result= totalWord.map(word=>word.replaceAll('X',''));
-    return [...new Set(result.sort())].indexOf(targetWord);
+    return count;
 }
-//AAAAA AAAAE AAAAI AAAAO AAAAU AAAAX  AAAEA AAAEE AAAEI AAAEO AAAEU
 
 
 
