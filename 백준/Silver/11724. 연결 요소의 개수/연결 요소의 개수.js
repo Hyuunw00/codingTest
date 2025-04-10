585a8351b5f0ecba33afc72a44a690682f280889
현우 <96560273+Hyuunw00@users.noreply.github.com>
2025-04-10 17:59:48 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 876 ms, Memory: 123324 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
@@ -21,24 +21,26 @@ for (let [node1, node2] of list) {
   graph[node2].push(node1);
 }
 
-let order = 1; //노드는 1개이상이므로 1번 노드부터 출발
 let count = 0; // 요소의 갯수
 const visited = {}; // 방문여부
+let queue = [];
 
-while (Object.keys(visited).length < N) {
-  if (!visited[order]) {
-    dfs(order);
+for (let i = 1; i <= N; i++) {
+  if (!visited[i]) {
+    bfs(i);
     count++;
   }
-  order++;
 }
 
-function dfs(node) {
-  visited[node] = true;
-
-  for (let neighbor of graph[node] || []) {
-    if (!visited[neighbor]) {
-      dfs(neighbor);
+function bfs(node) {
+  queue = [node];
+  while (queue.length) {
+    const node = queue.shift();
+    for (let neighbor of graph[node] || []) {
+      if (!visited[neighbor]) {
+        visited[neighbor] = true;
+        queue.push(neighbor);
+      }
     }
   }
 }
