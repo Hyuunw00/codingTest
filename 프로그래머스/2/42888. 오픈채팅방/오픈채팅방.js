c438db0dfa5855692b707e5f9e0020251b173437
현우 <96560273+Hyuunw00@users.noreply.github.com>
2025-05-10 15:14:15 +0900

    [level 2] Title: 오픈채팅방, Time: 246.27 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -0,0 +1,39 @@
+function solution(record) {
+    /*
+    1. 아이디어
+    유저의 아이디로 유저를 구분해야한다
+    record를 전부 돌았을때 유저의 아이디에 어떤 닉네임이 남았는지 저장한다.(객체에)
+    
+    다시 record를 돌면서 유저 아이디에 맞는 닉네임을 mapping해준다. 
+    결과에 출력되는 값은 Enter나 Leave일때만 남긴다.
+    
+    2. 시간복잡도
+    max:O(nlogn)
+    
+    split메서드는 시간복잡도가 평균적으로 n이지만 사용하는 곳의 최대갯수가 3이기때문에
+    n^2이여도 문제없음
+    
+    3. 자료구조
+    userid와 nickname을 담을 객체
+    result 배열
+    */
+    
+    const user= {};
+    
+    
+    record.forEach(el=>{
+       const [action,userId,nickname]= el.split(' ');
+       if(action!=='Leave') user[userId]= nickname;
+    })
+    
+    const result=[];
+    
+    record.forEach(el=>{
+      const [action,userId,nickname]= el.split(' ');
+      if(action!=='Change'){
+        const koreanAction = action==='Enter' ? '들어왔습니다.' : '나갔습니다.';
+        result.push(`${user[userId]}님이 ${koreanAction}`)
+      }
+    }) 
+    return result;
+}
