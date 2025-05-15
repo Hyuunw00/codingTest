21b7798af581aca9c7f8dc9d1644b36821aec28b
현우 <96560273+Hyuunw00@users.noreply.github.com>
2025-05-15 19:45:20 +0900

    [Silver I] Title: 미로 탐색, Time: 108 ms, Memory: 14680 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -19,7 +19,6 @@ import java.io.*;
 public class Main {
     public static int[][] map;
     public static int[][] visited;
-    public static int count= 1;
     public static int n, m;
     public static void main(String args[]) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
@@ -40,7 +39,7 @@ public class Main {
         }
         
         bfs(0,0);
-        System.out.println(count);
+        System.out.println(visited[n-1][m-1]);
         
     }
     public static void bfs(int y,int x){
@@ -52,8 +51,6 @@ public class Main {
         queue.offer(new int[] {y,x});
 
         while(!queue.isEmpty()){
-        int len= queue.size();
-            for(int i=0;i<len;i++){
                    int[] value= queue.poll();
                     int cury= value[0];
                     int curx =value[1];
@@ -61,16 +58,10 @@ public class Main {
                     int ny= cury + dy[j];
                     int nx= curx + dx[j];
                     if(nx>=0 && nx<m && ny>=0 && ny<n && visited[ny][nx]==0 && map[ny][nx]==1){
-                        if(nx==m-1 && ny==n-1) {
-                            count++;
-                            return;
-                        }
                         queue.offer(new int[] {ny,nx});
-                        visited[ny][nx]= 1;
-                   }
-                }       
-            }
-            count++;       
+                        visited[ny][nx]= visited[cury][curx] + 1;
+                   }   
+            }    
        }
      
         
