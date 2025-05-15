82f7e7971d36624ee0b9720b412af591416bdfe2
현우 <96560273+Hyuunw00@users.noreply.github.com>
2025-05-15 22:42:02 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 119.34 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 111 MB, 시간: 79.66 ms
+메모리: 97.8 MB, 시간: 119.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 19일 00:17:21
+2025년 05월 15일 22:41:58
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,33 +1,33 @@
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String result ="";
-        HashMap<String, Integer> map = new HashMap<>();
+        /*
+        1. 아이디어
+        참여자들을 돌면서 참여한 인원의 명수를 HashMap에 저장
+        완주한자들을 돌면서 HashMap에 있으면 --;
+        HashMap의 keySet을 돌면서 아직도 존재하면 그사람이 미완주자
         
-        for (String player : participant) {
-            map.put(player, map.getOrDefault(player, 0) + 1);
-        }
-
-       for (String player : completion) {
-            map.put(player, map.get(player)-1);
-        }
-
-        for(String key : map.keySet()){
-            if(map.get(key)!=0) {
-                result= key;
-                break;
-            }
+        2. 시간복잡도
+        O(logn)
+        3. 자료구조
+        HashMap
+        */
+        
+        HashMap<String,Integer> hm = new HashMap<>();
+        for(String p : participant){
+            if(hm.get(p)==null) hm.put(p,0);
+            hm.put(p,hm.get(p) +1);
         }
         
-        return result; 
-    }
+        for(String c : completion){
+            if(hm.get(c) > 0) hm.put(c,hm.get(c) - 1);
+        }
 
-    public static void main(String[] args) {
-        String[] participant = {"leo", "kiki", "eden"};
-        String[] completion = {"eden", "kiki"};
-
-        Solution sol = new Solution();
-        System.out.println(sol.solution(participant, completion));  // 메서드 호출
+        String result="";
+        for(String key : hm.keySet()){
+            if(hm.get(key)>0) result= key;
+        }
+        return result;
     }
 }
