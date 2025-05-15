5ce2fa430102bdcd551cc42310244efc5d3240c4
현우 <96560273+Hyuunw00@users.noreply.github.com>
2025-05-15 11:54:52 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 604 ms, Memory: 141648 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -3,6 +3,8 @@ import java.io.*;
 
 public class Main {
     
+    public static ArrayList<Integer>[] graph;
+    public static boolean[] visited;
     public static void main(String args[]) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st= new StringTokenizer(br.readLine());
@@ -10,37 +12,35 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         
-        List<List<Integer>> graph = new ArrayList<>();
+        graph = new ArrayList[n+1];
         for(int i=0;i<=n;i++){
-            graph.add(new ArrayList<>());
+            graph[i] = new ArrayList<>();
         }
-        
         for(int i=0;i<m;i++){
             st= new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            graph.get(start).add(end);
-            graph.get(end).add(start);
+            graph[start].add(end);
+            graph[end].add(start);
         }
-        boolean[] visited= new boolean[n+1];
+        visited= new boolean[n+1];
         int count=0;
          
         for(int i=1;i<=n;i++){
             if(!visited[i]){
-              dfs(graph, i, visited);
+              dfs(i);
               count++;  
             } 
         }
-        
         System.out.println(count);
 
     }
     
-    public static void dfs(List<List<Integer>> graph,int node,  boolean[] visited ) {
+    public static void dfs(int node) {
         visited[node] = true;
-        for(int i :graph.get(node)){
+        for(int i :graph[node]){
             if(!visited[i]){
-                dfs(graph, i, visited);
+                dfs(i);
             }
         }
     }
